26f4e29f6b2212ae89129f27e618df5f478de19e
Jason Brant <54822381+calijason76@users.noreply.github.com>
2019-10-08 18:32:07 -0400

    Update tab_3.py

--- tabs/tab_3.py
+++ tabs/tab_3.py
@@ -10,8 +10,8 @@ tab_3_layout = html.Div([
             html.H6('Select one:'),
             dcc.Slider(
                 id='page-3-slider',
-                min=1,
-                max=8,
+                min=4,
+                max=10,
                 step=0.1,
                 marks={i:str(i) for i in range(1, 9)},
                 value=5,
