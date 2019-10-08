1d58c8405841c05c4fd5f6d633893fa83d9a8be6
Jason Brant <54822381+calijason76@users.noreply.github.com>
2019-10-08 18:34:13 -0400

    Update tab_2.py

--- tabs/tab_2.py
+++ tabs/tab_2.py
@@ -4,7 +4,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 tab_2_layout = html.Div([
-    html.H1('Page 2'),
+    html.H1('Size'),
     html.Div([
         html.Div([
             html.H6('Select one:'),
